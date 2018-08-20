82987e7e6b64f4966054f281901144f0fa6c1b91
JacK <42542336+codydalton168@users.noreply.github.com>
2018-08-20 21:09:57 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
